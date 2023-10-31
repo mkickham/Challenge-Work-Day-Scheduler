282f149a76b8e2fd456a7f21d038a09f51791231
Michael Kickham <mfkickham3@gmail.com>
2023-10-31 17:07:32 -0500

    completed project and fixed 404 error

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// this pulls from the jQuery site
+// this makes sure that the page only loads when it's ready and also makes the save buton function
 $(document).ready(function(){
   $('.saveBtn').on("click", function(){
     var value = $(this).siblings('.description').val()
@@ -11,63 +8,41 @@ $(document).ready(function(){
     localStorage.setItem(id, value)
   });
 
+// this takes time-block ID and gives it local storage
 $('#hour-9 .description').val(localStorage.getItem('hour-9'))
 });
 $('#hour-10 .description').val(localStorage.getItem('hour-10'));
 $('#hour-11 .description').val(localStorage.getItem('hour-11'));
 $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-$('#hour-1 .description').val(localStorage.getItem('hour-1'));
-$('#hour-2 .description').val(localStorage.getItem('hour-2'));
-$('#hour-3 .description').val(localStorage.getItem('hour-3'));
-$('#hour-4 .description').val(localStorage.getItem('hour-4'));
-$('#hour-5 .description').val(localStorage.getItem('hour-5'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
+// this creates a function which is giving me the current time using dayjs
 function currentTime(){
   const currentTime = dayjs();
   const formattedTime = currentTime.format("YYYY-MM-DD HH:mm:ss");
-  $('#current-time').text(formattedTime);
+  $('#currentDay').text(formattedTime);
   }
+  // this is an if/else statement that is checkign the hour/current hour and giving the time-blocks color based on past/present/future classes
+  $( ".time-block" ).each(function(i) {
+    var hour = $(this).attr("id").split("hour-")[1];
+    var currentHour = dayjs().hour();
+    console.log(hour, currentHour)
+    if (currentHour == hour) {
+    $(this).addClass("present")
+    } else if (hour < currentHour) {
+    $( this ).addClass( "past" )
+    } else if (hour > currentHour) {
+    $(this).addClass("future") 
+    }
+  });
 
   currentTime();
 
-  setInterval(CurrentTime, 1000);
+  setInterval(currentTime, 1000);
 
 const localeSettings = {};
 dayjs.locale(localeSettings);
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
-  //
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  function hourColor(){
-// this is supposed to change the color for each of the blocks of past, present, future
-  }
-
-  function colorChange(){
-// this is supposed to dynamically update the color based on the time
-  }
-
-  function timeChange(){
-// this is supposed to control the time widget and update it dynamically
-  }
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
